4c5f750564b2e6f1f21cb62a7707482ca79a6f7c
Aman Kanojiya <amanskanojiya@gmail.com>
2024-03-29 23:24:52 +0530

    commit

--- programs/Naturalnumbersum.java
+++ programs/Naturalnumbersum.java
@@ -7,7 +7,13 @@ public class Naturalnumbersum {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter n : ");
         int  n = sc.nextInt();
-        int sum = (n*(n+1))/2;
+        int sum = (n*(n+1))/2; //formula for sum of first n natural numbers
         System.out.println("The sum of first "+n+ " natural numbers is : "+ sum);
     }
 }
+/*
+
+ Enter n : 5
+The sum of first 5 natural numbers is : 15 
+
+ */
