64eac32aed99938d2a34b4492e9eeaa2ebe10604
Aman Kanojiya <amanskanojiya@gmail.com>
2024-03-31 13:20:49 +0530

    commit

---
+++ programs/PrintASCII.java
@@ -0,0 +1,23 @@
+
+
+// Q. Print ASCII value of the character 
+
+package programs;
+
+import java.io.IOException;
+
+public class PrintASCII {
+    public static void main(String[] args) throws IOException {
+        System.out.print("Enter the character : ");
+        int num = System.in.read();
+        System.out.println(num);
+    }
+}
+
+/*
+ 
+Enter the character : a
+49s
+
+
+ */
